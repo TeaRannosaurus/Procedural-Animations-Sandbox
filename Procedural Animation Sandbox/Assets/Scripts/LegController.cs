9847d90fa150fe4b22fe8b13566d8d06e1bf5c81
Lars Kruik <lars.kruik@live.nl>
2021-02-12 14:18:52 +0100

    Add simple leg stepping

--- Procedural Animation Sandbox/Assets/Scripts/LegController.cs
+++ Procedural Animation Sandbox/Assets/Scripts/LegController.cs
@@ -8,21 +8,60 @@ public class LegController : MonoBehaviour
     [SerializeField] private Transform  HomeLocation;
     [SerializeField] private float      MaxStepDistance;
     [SerializeField] private float      MoveDuration;
-    [HideInInspector] public bool       IsMoving;
+    /*[HideInInspector]*/ public bool       IsMoving;
 
+    private void Update()
+    {
+        if (IsMoving) 
+            return;
+
+        float distanceFromHomeLocation = Vector3.Distance(transform.position, HomeLocation.position);
+
+        if (distanceFromHomeLocation > MaxStepDistance)
+            StartCoroutine(MoveToHomeLocation());
+    }
+
+    // Routine for moving the feet back in place
     private IEnumerator MoveToHomeLocation()
     {
         IsMoving = true;
 
         //Start position and rotation
+        Quaternion startRotation = transform.rotation;
+        Vector3 startPosition = transform.position;
 
         //End position and rotation
+        Quaternion endRotation = HomeLocation.rotation;
+        Vector3 endPosition = HomeLocation.position;
+
+        float elapsedTime = 0.0f;
 
         //Do loop till we reach goal within time
+        while (elapsedTime < MoveDuration)
+        {
+            // Time since last frame and normalized value to get step distance
+            elapsedTime += Time.deltaTime;
+            float normalizedTime = elapsedTime / MoveDuration;
 
-        //Stop moving
+            transform.position = Vector3.Lerp(startPosition, endPosition, normalizedTime);
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, normalizedTime);
 
-        yield return null;
+            yield return null;
+        }
+
+        IsMoving = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (IsMoving)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, 0.25f);
+        Gizmos.DrawLine(transform.position, HomeLocation.position);
+        Gizmos.DrawWireCube(HomeLocation.position, Vector3.one * 0.1f);
     }
 
 }
